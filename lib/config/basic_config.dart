bcbcac7afe93c396b90d991fb436cdf02fce8a96
YXM <375376810@qq.com>
2026-03-26 20:14:02 +0800

    sj

--- lib/config/basic_config.dart
+++ lib/config/basic_config.dart
@@ -1,5 +1,5 @@
 class BasicConfig {
     //服务器根地址
-    static const String basicServerUrl = "http://192.168.123.29:8080/spring_boot_jd_shop/";
+    static const String basicServerUrl = "http://192.168.123.190:8080/spring_boot_jd_shop/";
     //static const String basicServerUrl = "https://www.itying.com/";
 }
